a85c1a880fcf8910168e058ed6d6fdcbf5ac3ce5
Sofia <sofia.leivapires@gmail.com>
2025-11-12 08:38:24 -0300

    Atualização final

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ window.document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("btnMaiuscula").addEventListener("click", function(){
         let inputText = document.querySelector("#input-text").value
 
-        document.querySelector("#resultado").innerHTML = "Resultado: " + inputText.toUpperCase()
+        document.querySelector("#resultado-input").innerHTML = "Resultado: " + inputText.toUpperCase()
     } )
 
     document.getElementById("btnMinuscula").addEventListener("click", function(){
         let inputText = document.querySelector("#input-text").value
 
-        document.querySelector("#resultado").innerHTML = "Resultado: " + inputText.toLowerCase()
+        document.querySelector("#resultado-input").innerHTML = "Resultado: " + inputText.toLowerCase()
     } )
 
 
