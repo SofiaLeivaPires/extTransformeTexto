7adfe5bddd1654c66dee3844e4789d6a0755fee3
Sofia <sofia.leivapires@gmail.com>
2025-11-12 09:39:37 -0300

    feature 1 capitalizar

--- script.js
+++ script.js
@@ -11,5 +11,21 @@ window.document.addEventListener("DOMContentLoaded", function(){
         document.querySelector("#resultado-input").innerHTML = "Resultado: " + inputText.toLowerCase()
     } )
 
+    document.getElementById("btnPrimeiraMaiuscula").addEventListener("click", function(){
+        let inputText = document.querySelector("#input-text").value
+
+        // document.querySelector("#resultado-input").innerHTML = "Resultado: " + inputText.charAt(0).toUpperCase() + inputText.substring(1)
+
+        let palavras = inputText.split(" ")
+
+        for (let i = 0; i < palavras.length; i++) {
+            palavras[i] = palavras[i][0].toUpperCase() + palavras[i].substr(1);
+        }
+
+        document.querySelector("#resultado-input").innerHTML = "Resultado: " +  palavras.join(" ");
+
+    } )
+
+  
 
 })
